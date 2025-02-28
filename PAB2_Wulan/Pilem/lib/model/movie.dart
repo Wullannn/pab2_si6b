51833307e4c8d32c82ad73229f4b7e84fdd53575
Komariah Wulandari <komariahwulandari_2226240119@mhs.mdp.ac.id>
2025-02-28 10:57:08 +0700

    Update
    Detail_screen dan home_screen

--- PAB2_Wulan/Pilem/lib/model/movie.dart
+++ PAB2_Wulan/Pilem/lib/model/movie.dart
@@ -1,29 +1,29 @@
-class Movie{
+class Movie {
   final int id;
   final String title;
-  final String overView;
+  final String overview;
   final String posterPath;
   final String backdropPath;
   final String releaseDate;
   final double voteAverage;
-
   Movie({
     required this.id,
     required this.title,
-    required this.overView,
+    required this.overview,
     required this.posterPath,
     required this.backdropPath,
     required this.releaseDate,
-    required this.voteAverage});
-
-  factory Movie.fromJson(Map<String, dynamic>json){
+    required this.voteAverage
+  });
+  factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-        id: json ['id'],
-        title: json ['title'],
-        overView: json ['overView'],
-        posterPath: json ['posterPath'],
-        backdropPath: json ['backdropPath'],
-        releaseDate: json ['backdropPath'],
-        voteAverage: json ['voteAverage']);
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+      posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      releaseDate: json['release_date'],
+      voteAverage: json['vote_average'].toDouble(),
+    );
   }
 }
--- PAB2_Wulan/Pilem/lib/services/api_service.dart
+++ PAB2_Wulan/Pilem/lib/services/api_service.dart
@@ -1,29 +1,24 @@
 import 'dart:convert';
-
-class ApiService{
-  static const String baseUrl = "http://api.themoviedb.org/3";
-  static const String apiKey = '10c18dfa53630f23e70c0fa063de6456';
-
-  get http => null;
-
-  Future<List<Map<String, dynamic>>> getAllMovies() async{
+import "package:http/http.dart" as http;
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = '3f532695bc2452d24f8e1f6950944b9d';
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
     final response = await
     http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
-
-  Future<List<Map<String, dynamic>>> getTrendingMovies() async{
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
     final response = await
     http.get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
-
-  Future<List<Map<String, dynamic>>> getPopularMovies() async{
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
     final response = await
     http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
 }
