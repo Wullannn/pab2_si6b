f99653fa0b73d13b3abdf51637a04948396e32f4
Wulan <komariahwulandari_2226240119@mhs.mdp.ac.id>
2025-03-04 12:31:45 +0700

    update
    search_screen

--- PAB2_Wulan/Pilem/lib/screens/search_screen.dart
+++ PAB2_Wulan/Pilem/lib/screens/search_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:pilem/model/movie.dart';
 import 'package:pilem/services/api_service.dart';
@@ -17,7 +15,56 @@ class _SearchScreenState extends State<SearchScreen> {
   List<Movie> _searchResults = [];
 
   @override
+  void intiState(){
+    super.initState();
+    _searchController.addListener(_searchMovie);
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    _searchController.dispose();
+  }
+
+  void _searchMovie() async {
+    if(_searchController.text.isEmpty){
+      setState(() {
+        _searchController.clear();
+      });
+      return;
+    }
+    final List<Map<String, dynamic>> _searchData =
+        await _apiService.searchMovies(_searchController.text);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 1.0,
+                ),
+              ),
+              child: Row(
+                children: [
+                  
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
