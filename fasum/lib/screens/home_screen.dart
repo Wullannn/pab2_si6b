5526e077ec739b67903c8069f72f248dfe84eb5f
MichellePtrciaa <michellepatriciawijaya_2327240047@mhs.mdp.ac.id>
2025-05-20 13:01:22 +0700

    update

--- fasum/lib/screens/home_screen.dart
+++ fasum/lib/screens/home_screen.dart
@@ -6,6 +6,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import 'detail_screen.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
